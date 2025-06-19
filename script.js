92aa0e9bee14b6f0f67d7bd6b4d4d360a23c7987
Yamen Morcel <77121917+Yamen-M@users.noreply.github.com>
2025-06-19 10:33:58 +0300

    added UI and Styling

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const buttons = [
+    { value: '7', type: 'number' },
+    { value: '8', type: 'number' },
+    { value: '9', type: 'number' },
+    { value: '/', type: 'operator' },
+    { value: '4', type: 'number' },
+    { value: '5', type: 'number' },
+    { value: '6', type: 'number' },
+    { value: '*', type: 'operator' },
+    { value: '1', type: 'number' },
+    { value: '2', type: 'number' },
+    { value: '3', type: 'number' },
+    { value: '-', type: 'operator' },
+    { value: '0', type: 'number' },
+    { value: '.', type: 'number' },
+    { value: '=', type: 'equals' },
+    { value: '+', type: 'operator' },
+];
+
+const btnContainer = document.querySelector(".buttons")
+
+buttons.forEach(btn => {
+    const button = document.createElement('button');
+    button.textContent = btn.value;
+    button.setAttribute('data-value', btn.value);
+    button.classList.add('btn', `btn-${btn.type}`);
+    btnContainer.appendChild(button);
+});
+
+
 function add(a, b)
 {
     return a + b;
@@ -41,4 +71,5 @@ function operate(operator, val1, val2)
         default:
             alert("ERROR-SWITCH!");
     }
+    return result;
 }
